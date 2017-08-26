4cb990bc7f0ca59b731940088f7494c41017f76c
David Palmquist <dpalmquist84@gmail.com>
2017-08-26 12:50:20 -0400

    intial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("this is loaded");
+
+explorts.twitterKeys = {
+	consumer_key: aZ6MT2d5N03VjVuPHxVC7aTfh,
+	consumer_secret: 	Hl4PvmwLrkXM2etNniis7ghxD8tg9dtf6yv6oUDKqLy6XR0fxK,
+	access_token_key: 	901438579817185280-lVmKmbFASoels1bYyeKOjg69G9mSWP0,
+	access_token_secret: QpjDsLc4jkYczcwrjlEYv2nfhXSMcO21gF2ayWac26X2D,
+}
